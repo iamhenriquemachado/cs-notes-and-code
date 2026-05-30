40117de1de08a960646a07dd94fd73c8ba2420f9
henrique.castro <heyhenriquecastro@gmail.com>
2026-05-30 16:12:25 -0300

    feat: math tool kit to practice: method definition, overloading, parameters, arguments, static class and methods.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World!");
+﻿using cs_notes_and_code.challenges;
+
+double result1 = MathToolKit.Average(5, 10);
+Console.WriteLine($"Test 1 (2-Params): Expected 7.5 | Got: {result1}");
+
+double result2 = MathToolKit.Average(10, 20, 30);
+Console.WriteLine($"Test 2 (3-Params): Expected 20.0 | Got: {result2}");
+
+double[] prices = { 1.5, 2.5, 4.0, 6.0 };
+double result3 = MathToolKit.Average(prices);
+Console.WriteLine($"Test 3(Array): Expected 3.5 | Got: {result3}");
---
+++ challenges/MathToolKit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_notes_and_code.challenges
+{
+    internal class MathToolKit
+    {
+        public static double CalculateHypotenuse(double sideA, double sideB)
+        {
+            double triangleArea = sideA + sideB;
+            return Math.Sqrt(triangleArea);
+        }
+
+        public static double CalculatePercentage(double amount, double total = 100.0)
+        {
+            if (total == 0)
+            {
+                Console.WriteLine("The total should be greater than 0");
+            }
+            return (amount / total) * 100;
+
+        }
+
+        public static double Average(int a, int b)
+        {
+            return (a + b) / 2;
+        }
+
+        public static double Average(int a, int b, int c)
+        {
+            return (a + b + c) / 3;
+        }
+
+        public static double Average(double[] numbers)
+        {
+            double sumArray = numbers.Sum();
+            return sumArray / numbers.Length;
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a , int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int a , int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by 0");
+            }
+
+            else
+            {
+                return a / b;
+            }
+        }
+
+        public static double Potentiation(int a, int b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+
+
+}
