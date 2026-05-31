13ee691c52f53c622c56f889735533a4911eaf55
henrique.castro <heyhenriquecastro@gmail.com>
2026-05-31 17:05:03 -0300

    feat: new method for add new contacts

--- challenges/ContactBook/ContactBook.cs
+++ challenges/ContactBook/ContactBook.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cs_notes_and_code.challenges.ContactBook
 {
     internal class ContactBook
     {
+        public void AddContact(List<Contact> myContacts, string name, string phone, string? email)
+        {
+            Contact newContact = new Contact
+            {
+                Name = name,
+                Phone = phone,
+                Email = email ?? "No Email Provided"
+            };
 
-       
+            myContacts.Add(newContact);
+        }
+
         public void ListContacts(List<Contact> myContacts)
         {
-            foreach(var contact in myContacts)
+            foreach (var contact in myContacts)
             {
-                Console.WriteLine($"List of contacts: {contact.Name},s {contact.Email}, {contact.Phone}");
+                Console.WriteLine($"Name: {contact.Name}, Email: {contact.Email}, Phone: {contact.Phone}");
             }
         }
     }
