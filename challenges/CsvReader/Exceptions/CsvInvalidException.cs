0fd6c34e77535f12b2b94d776d7d77e84d974c26
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-04 13:26:00 -0300

    feat: implemented invalid custom exception for csv reading

---
+++ challenges/CsvReader/Exceptions/CsvInvalidException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_notes_and_code.challenges.CsvReader.Exceptions
+{
+    internal class CsvInvalidException : Exception
+    {
+        public int ViolatingValue { get; set; }
+        public int RuleName { get; set; }
+
+        public CsvInvalidException(string message) : base(message) { }
+
+        public CsvInvalidException(string message, int violatingValue, int ruleName) : base($"{message} (Value violated at: {violatingValue}")
+        {
+            ViolatingValue = violatingValue;
+            RuleName = ruleName;
+        }
+
+    }
+}
