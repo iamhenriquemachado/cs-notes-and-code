780c590adf1a12fdc7670a3f9cee9dbc60812fe3
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-04 22:51:35 -0300

    feat: finished delete method to create a new list after removing the given id

--- challenges/CsvReader/CsvBuilder.cs
+++ challenges/CsvReader/CsvBuilder.cs
@@ -2,29 +2,18 @@
 using cs_notes_and_code.challenges.CsvReader.Entities;
 using cs_notes_and_code.challenges.CsvReader.Exceptions;
 using cs_notes_and_code.challenges.CsvReader.Interfaces;
-using cs_notes_and_code.challenges.GenericsMemoryRepository;
-using CsvHelper;
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace cs_notes_and_code.challenges.CsvReader
 {
     internal class CsvBuilder : ICar
     {
-
         private List<Car> _records = new List<Car>();
         string filePath = @"C:\Users\heyhe\development\cs-notes-and-code\challenges\CsvReader\Files\entity.csv";
-
         public void Add(List<Car> cars)
         {
-
             if (cars == null) throw new ArgumentNullException("The list cannot be null...");
-
             try
             {
                 Console.WriteLine("Inserting new cars on the list");
@@ -32,14 +21,9 @@ namespace cs_notes_and_code.challenges.CsvReader
                 {
                     if (car.Year < 2000)
                     {
-                        throw new CsvInvalidException(
-                            message: $"Cannot insert a new card record before 2000.",
-                            violatingValue: car.Year,
-                            ruleName: 101
-                         );
+                        throw new CsvInvalidException(message: $"Cannot insert a new card record before 2000.", violatingValue: car.Year, ruleName: 101);
                     }
                 }
-
                 using (var writer = new StreamWriter(filePath, true))
                 using (var csv = new CsvHelper.CsvWriter(writer, CultureInfo.InvariantCulture))
                 {
@@ -52,29 +36,25 @@ namespace cs_notes_and_code.challenges.CsvReader
                     {
                         csv.WriteRecord(car);
                         csv.NextRecord();
-
                     }
                     _records.AddRange(cars);
-                }
+                    Console.WriteLine($"Records inserted on the list... {_records.Count()}");
 
+                }
             }
             catch (CsvInvalidException e)
             {
                 Console.WriteLine($"CSV invalid exception: {e.ViolatingValue}");
             }
-
         }
-
         public void DeleteById(int id)
         {
             try
             {
-
-                using (var reader = new StreamReader(@"C:\Users\heyhe\development\cs-notes-and-code\challenges\CsvReader\Files\entity.csv", true))
+                using (var reader = new StreamReader(filePath, true))
                 using (var csv = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture))
                 {
                     _records = csv.GetRecords<Car>().ToList();
-
                     for (int i = 0; i < _records.Count; ++i)
                     {
                         if (_records[i].Id == id)
@@ -83,27 +63,26 @@ namespace cs_notes_and_code.challenges.CsvReader
                         }
                     }
                 }
-
-                // Needs to rewrite the file again after deleting by id
-
+                using (var writer = new StreamWriter(filePath, true))
+                using (var csv = new CsvHelper.CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(_records);
+                }
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
-
         public IReadOnlyList<Car> List()
         {
-            using (var reader = new StreamReader(@"C:\Users\heyhe\development\cs-notes-and-code\challenges\CsvReader\Files\entity.csv"))
+            using (var reader = new StreamReader(filePath))
             using (var csvReader = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 var result = csvReader.GetRecords<Car>().ToList();
                 return result;
             }
         }
-
         public void SearchById(int id)
         {
             throw new NotImplementedException();
