c45ea45f146693020a4a535bdb075a6cf97ce557
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-08 14:51:17 -0300

    feat: event arguments

---
+++ challenges/Day 21-22/FuncEventHandler/Events/ProcessingProgressEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_notes_and_code.challenges.Day_21_22.FuncEventHandler.Events
+{
+    internal class ProcessingProgressEventArgs : EventArgs
+    {
+
+        public int PercentComplete { get; }
+        public string CurrentBatchName { get; }
+
+        public ProcessingProgressEventArgs(int percentComplete, string currentBatchName)
+        {
+            PercentComplete = percentComplete;
+            CurrentBatchName = currentBatchName;
+        }
+    }
+}
--- challenges/Day 21-22/FuncEventHandler/FleetProcessor.cs
+++ challenges/Day 21-22/FuncEventHandler/FleetProcessor.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using cs_notes_and_code.challenges.Day_21_22;
 
 namespace cs_notes_and_code.challenges.Day_21_22.FuncEventHandler
 {
     internal class FleetProcessor : IVehicleParser
     {
-
+        List<Vehicle> _vehicles = new List<Vehicle>();
         public void RawDataParser(string[] rawData)
         {
 
@@ -14,21 +15,25 @@ namespace cs_notes_and_code.challenges.Day_21_22.FuncEventHandler
             {
                 var splittedList = item.Split(',');
 
+                if (splittedList.Length < 5)
+                {
+                    throw new ArgumentNullException("List is corrupted");
+                }
+
                 var normalizeYear = Int32.TryParse(splittedList[2], out int year);
                 var normalizeMileage = Int32.TryParse(splittedList[3], out int mileage);
                 var normalizeStatus = bool.TryParse(splittedList[4], out bool status);
 
-                List<Vehicle> vehicleList = new()
+                Vehicle vehicle = new Vehicle()
                 {
-                    new Vehicle
-                    {
-                        Brand = splittedList[0],
-                        Model = splittedList[1],
-                        Year = year,
-                        Mileage = mileage,
-                        Active = status
-                    }
+                    Brand = splittedList[0],
+                    Model = splittedList[1],
+                    Year = year,
+                    Mileage = mileage,
+                    Active = status
                 };
+
+                _vehicles.Add(vehicle);
             }
         }
     }
