98a3a5ae21761e6e80e97880c12d1cb73c9abff2
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-08 14:50:59 -0300

    feat: built data invalid custom exception

---
+++ challenges/Day 21-22/FuncEventHandler/Exceptions/InvalidVehicleDataException .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_notes_and_code.challenges.Day_21_22.FuncEventHandler.Exceptions
+{
+    internal class InvalidVehicleDataException : Exception
+    {
+        public int RawLine { get; set; }
+        public string ValidationRule { get; set; }
+
+        public InvalidVehicleDataException(string message) : base(message) { }
+
+        public InvalidVehicleDataException(int rawLine, string validationRule, string message) : base($"Message: {message} | Validation Rule Broken: {validationRule}")
+        {
+            RawLine = rawLine;
+            ValidationRule = validationRule;
+
+        }
+    }
+}
